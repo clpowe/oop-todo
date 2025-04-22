0e214fe8057e4f7a6826803068c490369e950173
clpowe <clpowe@gmail.com>
2025-04-22 15:07:56 -0400

    updated main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+
+	"github.com/clpowe/oop-todo/internal/handler"
+	"github.com/clpowe/oop-todo/internal/repository"
+	"github.com/clpowe/oop-todo/internal/service"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	engine := html.New("internal/templates", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Static("/static", "./web/static")
+
+	repo := repository.NewInMemoryRepo()
+	svc := service.NewTodoService(repo)
+	h := handler.NewTodoHandler(svc)
+
+	h.RegisterRoutes(app)
+
+	log.Println("Starting server on :8080")
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatalf("Server error: %s", err)
+	}
 }
